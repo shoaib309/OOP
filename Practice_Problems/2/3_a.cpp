006a5dcac6105d832fce540b179d6bd84beeecbc
shoaib <p200147@pwr.nu.edu.pk>
2021-09-15 16:14:47 +0500

    Adding the 3_a.cpp file

---
+++ Practice_Problems/2/3_a.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <stdio.h>
+#include <conio.h>
+using namespace std;
+
+void swap_using_pass_by_value(int x, int y)
+{
+
+
+    int temp;
+
+    temp = x;
+    x = y;
+    y = temp;
+
+}
+
+
+int main()
+{
+
+    int A = 10, B=25 ;
+
+    cout<<"Before swapping:"<<endl;
+    cout<<"A = "<<A<<endl;
+    cout<<"B = "<<B<<endl;
+
+    swap_using_pass_by_value(A,B);
+
+    cout<<"After swapping:"<<endl;
+    cout<<"A = "<<A<<endl;
+    cout<<"B = "<<B<<endl;
+
+
+
+
+
+
+
+}
